0fa99dda1c16934f2ff8b0b862b010946ee77764
Dongdong Kong <kongdd@users.noreply.github.com>
2025-10-28 17:42:42 +0800

    add tests

--- tests/test_noahpy.py
+++ tests/test_noahpy.py
@@ -3,22 +3,23 @@ using this file to test NoahPy and NoahPy_Module
 run this program, NoahPy_Module_ouput.csv and NoahPy_output.csv will be created
 """
 import NoahPy
+import os.path
+
+f_forcing = os.path.abspath("data/forcing.txt")
 
 
 def test_NoahPy():
     from NoahPy.NoahPy_Module import NoahPy
 
     model = NoahPy()
-    Date, STC, SH2O = model.noah_main(
-        "data/forcing.txt", output_flag=True, lstm_model=None
-    )
+    Date, STC, SH2O = model.noah_main(f_forcing, output_flag=True, lstm_model=None)
 
 
 def test_NoahPy_module():
     from NoahPy.NoahPy import noah_main
 
-    Date, STC, SH2O = noah_main("data/forcing.txt", output_flag=True, lstm_model=None)
+    Date, STC, SH2O = noah_main(f_forcing, output_flag=True, lstm_model=None)
 
 
-test_NoahPy_module()
-test_NoahPy()
+# test_NoahPy_module()
+# test_NoahPy()
